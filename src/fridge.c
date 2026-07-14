124aa8738b6565ea6ff126de1fc8e4937e9945ec
Pyrix25633 <biral.mattia@gmail.com>
2026-07-14 20:54:35 +0200

    Solved problem about reads and writes

--- src/fridge.c
+++ src/fridge.c
@@ -42,6 +42,9 @@ bool force_exit = false;
 
 struct sigaction action_handler;
 
+// TODO: implement autoclose
+// TODO: organize code consistently
+
 int main(int argc, char *argv[]) {
     id = get_id_from_arguments(argc, argv);
 
@@ -63,65 +66,64 @@ int main(int argc, char *argv[]) {
         if(read(rcv_requests_fd, request_buffer, MAX_REQUEST_SIZE) < 0 || printf("Read: %s\n", request_buffer) < 0) {
             response.command_code = NULL_COMMAND; // Could not be parsed
             response.response_code = UNABLE_TO_READ_PIPE;
-            printf("Read error\n"); // ! remove
         }
         else if((error_code = parse_request(&request, request_buffer, MAX_REQUEST_SIZE)) != OK) {
             response.command_code = NULL_COMMAND; // Could not be parsed
             response.response_code = error_code;
-            printf("Parse error: %u\n", error_code); // ! remove
         }
         else {
             response.command_code = request.command_code;
             response.response_code = OK;
             response.arguments_size = 0;
-            printf("Received command: %u\n", request.command_code); // ! remove
             if(pthread_mutex_lock(&data_mutex) < 0) {
                 response.response_code = UNABLE_TO_LOCK_MUTEX;
-            } else if(request.destination != id) {
-                response.response_code = DESTINATION_ID_MISMATCH;
-            } else if(IS_INFO(request.command_code)) {
-                response.arguments_size = 6;
-                response.arguments[STATE_ARGUMENT] = state;
-                response.arguments[OPEN_HOURS_ARGUMENT] = calculate_seconds_open() / (60*60);
-                response.arguments[AUTOCLOSE_DELAY_ARGUMENT] = autoclose_delay;
-                response.arguments[FILL_PERCENTAGE_ARGUMENT] = fill_percentage;
-                response.arguments[THERMOSTAT_ARGUMENT] = thermostat;
-                response.arguments[TEMPERATURE_ARGUMENT] = calculate_current_temperature();
-            } else if(IS_LINK(request.command_code) && LINK_SUBCOMMAND(request.command_code) == LINK_CHANGE_PARENT) {
-                response.response_code = change_snd_responses_pipe(request.argument, &snd_responses_fd);
-            } else if(IS_DELETE(request.command_code)) {
-                // TODO: check if there are other things to do
-                force_exit = true;
-            } else if(IS_REGISTRY(request.command_code)) {
-                if(REGISTRY_SUBCOMMAND(request.command_code) == REGISTRY_DELAY) {
-                    autoclose_delay = request.argument;
-                } else if(REGISTRY_SUBCOMMAND(request.command_code) == REGISTRY_THERMOSTAT) {
-                    // TODO: validate thermostat
-                    thermostat = request.argument;
-                } else if(REGISTRY_SUBCOMMAND(request.command_code) == REGISTRY_PERCENTAGE) {
-                    if(request.argument <= 100) {
-                        fill_percentage = request.argument;
+            } else {
+                if(request.destination != id) {
+                    response.response_code = DESTINATION_ID_MISMATCH;
+                } else if(IS_INFO(request.command_code)) {
+                    response.arguments_size = 6;
+                    response.arguments[STATE_ARGUMENT] = state;
+                    response.arguments[OPEN_HOURS_ARGUMENT] = calculate_seconds_open() / (60*60);
+                    response.arguments[AUTOCLOSE_DELAY_ARGUMENT] = autoclose_delay;
+                    response.arguments[FILL_PERCENTAGE_ARGUMENT] = fill_percentage;
+                    response.arguments[THERMOSTAT_ARGUMENT] = thermostat;
+                    response.arguments[TEMPERATURE_ARGUMENT] = calculate_current_temperature();
+                } else if(IS_LINK(request.command_code) && LINK_SUBCOMMAND(request.command_code) == LINK_CHANGE_PARENT) {
+                    response.response_code = change_snd_responses_pipe(request.argument, &snd_responses_fd);
+                } else if(IS_DELETE(request.command_code)) {
+                    // TODO: check if there are other things to do
+                    force_exit = true;
+                } else if(IS_REGISTRY(request.command_code)) {
+                    if(REGISTRY_SUBCOMMAND(request.command_code) == REGISTRY_DELAY) {
+                        autoclose_delay = request.argument;
+                    } else if(REGISTRY_SUBCOMMAND(request.command_code) == REGISTRY_THERMOSTAT) {
+                        // TODO: validate thermostat
+                        thermostat = request.argument;
+                    } else if(REGISTRY_SUBCOMMAND(request.command_code) == REGISTRY_PERCENTAGE) {
+                        if(request.argument <= 100) {
+                            fill_percentage = request.argument;
+                        } else {
+                            response.response_code = INVALID_REQUEST_ARGUMENT;
+                        }
+                    }
+                } else if(IS_SWITCH(request.command_code)) {
+                    if(SWITCH_LABEL(request.command_code) == SWITCH_OPEN) {
+                        if(SWITCH_POSITION(request.command_code) == POSITION_ON) {
+                            set_state(STATE_OPEN);
+                        }
+                    } else if(SWITCH_LABEL(request.command_code) == SWITCH_CLOSE) {
+                        if(SWITCH_POSITION(request.command_code) == POSITION_ON) {
+                            set_state(STATE_CLOSED);
+                        }
                     } else {
-                        response.response_code = INVALID_REQUEST_ARGUMENT;
-                    }
-                }
-            } else if(IS_SWITCH(request.command_code)) {
-                if(SWITCH_LABEL(request.command_code) == SWITCH_OPEN) {
-                    if(SWITCH_POSITION(request.command_code) == POSITION_ON) {
-                        set_state(STATE_OPEN);
-                    }
-                } else if(SWITCH_LABEL(request.command_code) == SWITCH_CLOSE) {
-                    if(SWITCH_POSITION(request.command_code) == POSITION_ON) {
-                        set_state(STATE_CLOSED);
+                        response.response_code = INVALID_COMMAND;
                     }
                 } else {
                     response.response_code = INVALID_COMMAND;
                 }
-            } else {
-                response.response_code = INVALID_COMMAND;
-            }
-            if(pthread_mutex_unlock(&data_mutex) < 0) {
-                print_error(STDERR_FILENO, UNABLE_TO_UNLOCK_MUTEX, id, "while processing request");
+                if(pthread_mutex_unlock(&data_mutex) < 0) {
+                    print_error(STDERR_FILENO, UNABLE_TO_UNLOCK_MUTEX, id, "while processing request");
+                }
             }
         }
         // TODO: wait a random time
@@ -158,7 +160,10 @@ void set_state(leaf_device_state_t new_state) {
 }
 
 u_int32_t calculate_seconds_open() {
-    return seconds_open + (time(NULL) - last_opened);
+    if(state == STATE_OPEN) {
+        return seconds_open + (time(NULL) - last_opened);
+    }
+    return seconds_open;
 }
 
 u_int8_t calculate_current_temperature() {
--- src/headers/messages.h
+++ src/headers/messages.h
@@ -75,12 +75,12 @@ typedef struct response_t {
 
 #define IS_INFO(c)                (c & COMMAND_MASK) == INFO
 #define IS_LINK(c)                (c & COMMAND_MASK) == LINK
-#define LINK_SUBCOMMAND(c)        c & LINK_MASK
+#define LINK_SUBCOMMAND(c)        (c & LINK_MASK)
 #define IS_SWITCH(c)              (c & COMMAND_MASK) == SWITCH
-#define SWITCH_LABEL(c)           c & SWITCH_MASK
-#define SWITCH_POSITION(c)        c & POSITION_MASK
+#define SWITCH_LABEL(c)           (c & SWITCH_MASK)
+#define SWITCH_POSITION(c)        (c & POSITION_MASK)
 #define IS_REGISTRY(c)            (c & COMMAND_MASK) == REGISTRY
-#define REGISTRY_SUBCOMMAND(c)    c & REGISTRY_MASK
+#define REGISTRY_SUBCOMMAND(c)    (c & REGISTRY_MASK)
 #define IS_DELETE(c)              (c & COMMAND_MASK) == DELETE
 #define HAS_COMMAND_ARGUMENT(c)   (c & COMMAND_ARGUMENT_FLAG) == COMMAND_ARGUMENT_FLAG
 #define HAS_RESPONSE_ARGUMENTS(c) (c & RESPONSE_ARGUMENTS_FLAG) == RESPONSE_ARGUMENTS_FLAG
