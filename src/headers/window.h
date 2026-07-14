65b0b8753f24dac59f7c574d7511e57074489157
elisa335 <elisa332005@gmail.com>
2026-07-14 19:29:35 +0200

    Starting window development

--- src/headers/window.h
+++ src/headers/window.h
@@ -5,6 +5,22 @@
 #ifndef DOMOTICS_WINDOW_H
 #define DOMOTICS_WINDOW_H
 
+#include "return_codes.h"
+#include "messages.h"
+#include "utils.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+
+// Default values
+
+#define INITIAL_SECONDS_OPEN    0
+#define MAX_WINDOW_ARGUMENTS    2
+
+//TODO values to substitute, just placeholders for now
+#define DESTINATION_ID_MISMATCH   0x11
+
 /**
  * Main function of the Window Program
  * @param argc Number of arguments received
@@ -16,5 +32,25 @@
  * @returns `OK`
  */
 int main(int argc, char *argv[]);
+
+/**
+ * Handles the shutdown also cleaning up IPC files, best practice to do
+ */
+void handle_shutdown();
+
+/**
+ * Reads the pipe and identifies the command
+ */
+error_code_t read_pipe();
+
+/**
+ * Executes the command received from the pipe
+ */
+void execute_command();
+
+/**
+ * TODO
+ */
+void info_response();
 
 #endif
