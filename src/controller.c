8e1ab3d32d04554ee4be4664e921df0144b116c5
Pyrix25633 <biral.mattia@gmail.com>
2026-08-25 18:46:21 +0200

    Controller, removed sleep for list

--- src/controller.c
+++ src/controller.c
@@ -442,7 +442,6 @@ error_code_t check_user_command(user_command_t *user_command) {
 error_code_t execute_user_command(user_command_t *user_command) {
     if(IS_MESSAGE(user_command->code)) {
         if(user_command->code == INFO_COMMAND && user_command->target == id) {
-            simulate_processing_time();
             int32_t n = execute_list_command(false);
             if(IS_RETURN_ERROR(n)) {
                 print_error(STDERR_FILENO, ERROR_FROM_RETURN(n), id, "while listing devices");
